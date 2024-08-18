939d15789b6e7980eb172e3403e4e6172d6a4ecc
Balvant Kushwaha <140886238+Balvant4@users.noreply.github.com>
2024-08-19 01:58:23 +0530

    Update sendVerificationEmail.ts

--- src/helpers/sendVerificationEmail.ts
+++ src/helpers/sendVerificationEmail.ts
@@ -9,7 +9,7 @@ export async function sendVerificationEmail(
 ): Promise<ApiResponse> {
   try {
     await resend.emails.send({
-      from: "Balvant Portfolio <balvant@shop.com>",
+      from: "BalvantPortfolio@balvant.shop",
       to: email,
       subject: "Balvant Portfolio || Verification code",
       react: VerificationEmail({ username, otp: verifyCode }),
