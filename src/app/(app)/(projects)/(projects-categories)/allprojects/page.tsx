2ceeb529e1df34d328df29f91b1c853cd5d882e4
Balvant4 <balvantkushwaha44@gmail.com>
2024-08-20 01:41:36 +0530

    lorem add

--- src/app/(app)/(projects)/(projects-categories)/allprojects/page.tsx
+++ src/app/(app)/(projects)/(projects-categories)/allprojects/page.tsx
@@ -3,7 +3,12 @@ import React from "react";
 function page() {
   return (
     <div>
-      <h1>allprojects</h1>
+      <h1>
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Maiores fugit,
+        corporis expedita vitae odit laborum, dignissimos quas incidunt
+        repellendus et unde! Culpa, a labore hic atque molestiae vitae odio
+        eligendi!
+      </h1>
     </div>
   );
 }
